5e197c96624eec595e66037b1966b7b805744d2e
Adithya M S <47377931+Adithya-Meda@users.noreply.github.com>
2026-09-24 23:24:28 +0530

    fix: repair build.py to properly update README with blueprint_final.svg and dynamic cache buster

--- build.py
+++ build.py
@@ -271,14 +271,18 @@ if __name__ == "__main__":
         generate_repo_blueprint(repo['name'], repo['desc'], is_centered_wide)
         print("Generated unified main SVG and repo cards!")
 
-import build
 
-readme = f"""<div align="center">
-<img src="./assets/blueprint_main.svg?v=17" style="display: block; margin: 0; padding: 0; border: none; outline: none;" />
-<a href="https://github.com/Adithya-Meda/wisebiz-ecommerce-app"><img src="./assets/repo_wisebiz-ecommerce-app.svg?v=17" alt="AWS Landing Zone Resource" style="vertical-align: top;"></a><a href="https://github.com/Adithya-Meda/wisebiz-gitops"><img src="./assets/repo_wisebiz-gitops.svg?v=17" alt="K8s GitOps Resource" style="vertical-align: top;"></a>
-<a href="https://github.com/Adithya-Meda/wisebiz-terraform" style="display: block;"><img src="./assets/repo_wisebiz-terraform.svg?v=17" alt="Wisebiz Terraform Resource" style="display: block;"></a>
-</div>"""
+    import time
+    v = int(time.time())
+    
+    readme = f'''<div align="center">
+<img src="./assets/blueprint_final.svg?v={v}" style="display: block; margin: 0; padding: 0; border: none; outline: none;" />
 
-with open("README.md", "w", encoding="utf-8") as f:
-    f.write(readme)
+<!-- INTERACTIVE DEPLOYMENTS (REPO CARDS) -->
+<a href="https://github.com/Adithya-Meda/wisebiz-ecommerce-app"><img src="./assets/repo_wisebiz-ecommerce-app.svg?v={v}" alt="AWS Landing Zone Resource" style="vertical-align: top;"></a><a href="https://github.com/Adithya-Meda/wisebiz-gitops"><img src="./assets/repo_wisebiz-gitops.svg?v={v}" alt="K8s GitOps Resource" style="vertical-align: top;"></a>
+<a href="https://github.com/Adithya-Meda/wisebiz-terraform"><img src="./assets/repo_wisebiz-terraform.svg?v={v}" alt="Wisebiz Terraform Resource" style="display: block;"></a>
+</div>
+'''
 
+    with open("README.md", "w", encoding="utf-8") as f:
+        f.write(readme)
